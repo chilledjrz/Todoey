7a14e388fdf056bfc466bc46a4f61167d0be6c6b
admin <admin@192-168-1-7.tpgi.com.au>
2019-02-23 23:20:54 +1100

    add new item functionality complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     
     
@@ -56,7 +56,33 @@ class TodoListViewController: UITableViewController {
     }
     
     
-    
+    //- MARK - Add new Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on our ui alert
+           
+            self.itemArray.append (textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "create new item"
+            textField = alertTextField
+            
+            
+        }
+        
+        
+        alert.addAction(action)
+        
+        present(alert,animated: true, completion: nil)
+        
+        
+    }
     
     
 }
