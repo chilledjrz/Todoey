aa7c419a9aa54a4aeb97eb97b916581c72fe3440
admin <admin@192-168-1-7.tpgi.com.au>
2019-02-23 23:44:38 +1100

    data persisted using user defaults method

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,14 +11,17 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
-    
+    let defaults = UserDefaults.standard
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Do any additional setup after loading the view, typically from a nib.
         
-        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        itemArray = items
+            
+        }
         
         
     }
@@ -66,6 +69,9 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user clicks the add item button on our ui alert
            
             self.itemArray.append (textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
             
         }
